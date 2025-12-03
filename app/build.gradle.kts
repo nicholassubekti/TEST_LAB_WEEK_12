f7d90446e8518813c3fd302736878a555535eee0
Samuel Nicholas <nicholassubekti09@gmail.com>
2025-12-03 22:10:03 +0700

    Commit No.2 - Building app with coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,30 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.test_lab_week_12"
     compileSdk = 36
+
+    val localProperties = Properties()
+    val localPropertiesFile = File(rootDir, "local.properties")
+    if(localPropertiesFile.exists() && localPropertiesFile.isFile) {
+        localPropertiesFile.inputStream().use {
+            localProperties.load(it)
+        }
+    }
+
+    val tmdbApiKey = localProperties.getProperty("TMDB_API_KEY") ?: error("TMDB API Key not found in local.properties file")
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+        resValues = true
+    }
 
     defaultConfig {
         applicationId = "com.example.test_lab_week_12"
@@ -25,6 +43,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "TMDB_API_KEY", "$tmdbApiKey")
+        }
+        debug {
+            buildConfigField("String", "TMDB_API_KEY", "$tmdbApiKey")
         }
     }
     compileOptions {
@@ -41,10 +63,13 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.glide)
     implementation(libs.retrofit)
-    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    implementation("com.squareup.moshi:moshi")
+    implementation("com.squareup.moshi:moshi-kotlin")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
-    kapt(libs.moshi.kotlin.codegen)
+    ksp(libs.moshi.kotlin.codegen)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
